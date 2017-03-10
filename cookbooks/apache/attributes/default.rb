24a77c8e92b9229727ba2bf234a1b14d0dfaa677
raghu <k.r.rao@outlook.com>
2017-03-10 08:00:42 +0530

    updted attributes in apache

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,1 @@
+force_default["apache"]["indexfile"] = "index1.html"
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -26,7 +26,8 @@ end
 service "apache2" do
 action [ :enable, :start ]
 end
+node.force_default["apache"]["indexfile"] = "index2.html"
 cookbook_file "/var/www/html/index.html" do
-source "index.html"
+source node["apache"]["indexfile"]
 mode "0644"
 end
